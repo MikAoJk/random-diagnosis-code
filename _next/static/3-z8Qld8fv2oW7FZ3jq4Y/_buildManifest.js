4c9006c9443d1ff30c7dbdc7392a0c8297123272
MikAoJk <joakimkartveit@gmail.com>
2023-03-13 07:19:16 +0000

    Deploying to gh-pages from @ MikAoJk/random-diagnosis-code@d64b4390629804d09f18d42bc94bb3d8720c40b6 🚀

--- _next/static/3-z8Qld8fv2oW7FZ3jq4Y/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f6a210dd3f5e2b44.css","static/chunks/pages/index-b1a4ba581f5cad86.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/eR1_riuhpR8AlbDPyaC6k/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f6a210dd3f5e2b44.css","static/chunks/pages/index-d777e3ee84afc47b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
