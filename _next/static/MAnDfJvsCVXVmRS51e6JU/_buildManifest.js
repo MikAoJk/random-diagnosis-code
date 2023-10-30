f5a7e15e9398c9d7d70113748ccb839be1587a12
MikAoJk <joakimkartveit@gmail.com>
2023-10-30 05:00:17 +0000

    Deploying to gh-pages from @ MikAoJk/random-diagnosis-code@8e58bda85b3cca8feadd033dc004aa11fc25e7cc 🚀

---
+++ _next/static/MAnDfJvsCVXVmRS51e6JU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/c882f945a11842ef.css","static/chunks/pages/index-82c51f9257413faa.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
