fb553778caa9ad1a21d37eab8e1c5748431b7426
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2024-07-16 06:18:28 +0200

    Update RandomDiagnosisCode.tsx

--- src/components/RandomDiagnosisCode/RandomDiagnosisCode.tsx
+++ src/components/RandomDiagnosisCode/RandomDiagnosisCode.tsx
@@ -12,18 +12,18 @@ export interface Diagnose {
 
 const RandomDiagnosisCode = () => {
 
-    const [randomDiagnosisCode, setRandomDiagnosisCode] = useState("");
+    const [randomDiagnosisCode, setRandomDiagnosisCode] = useState<Diagnose>(null);
 
 
     const findRandomDiagnosisCode = async () => {
         const randomDiagnose = findRandomDiagnose()
-        setRandomDiagnosisCode(`Code: ${randomDiagnose.code} Text: ${randomDiagnose.text}`);
+        setRandomDiagnosisCode(randomDiagnose);
     }
 
     return (
         <div className="flex min-h-screen flex-col p-16 md:items-center md:p-24">
                 <Button variant="primary" size="medium" onClick={findRandomDiagnosisCode}>Random</Button>
-                {randomDiagnosisCode && <Heading size="large" level="1" spacing> {randomDiagnosisCode} </Heading>}
+                {randomDiagnosisCode && <Heading size="large" level="1" spacing>{'Code: ${randomDiagnose.code} Text: ${randomDiagnose.text}'}}
         </div>
     );
 };
